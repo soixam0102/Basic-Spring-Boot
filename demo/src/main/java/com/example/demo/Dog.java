a2f161178bb1e85d4b9e445c4a090ae3e671c5da
soixam0102 <sanamata085@gmail.com>
2021-08-24 02:21:35 +0700

    all

---
+++ demo/src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog {
+    public void speak() {
+        System.out.println("WWOO UWOO");
+    }
+}
